36987d7a4e65951cf4df73d67b3f4df0fe870eb9
Renato Farias <renatocdfs@gmail.com>
2025-05-30 23:23:38 +0100

    chaging host default when dockering it - fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
-HTTP_HOST = int(os.getenv("HTTP_HOST", "localhost"))
+HTTP_HOST = os.getenv("HTTP_HOST", "localhost")
 HTTP_PORT = int(os.getenv("HTTP_PORT", 8080))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "puchnstore")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "puchnstore")
