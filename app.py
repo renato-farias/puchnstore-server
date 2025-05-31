cc9528c0cc879998ad7efb16f5c619e18e41aaa0
Renato Farias <renatocdfs@gmail.com>
2025-05-31 14:21:38 +0100

    make secure_filename toggleable

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ HTTP_HOST = os.getenv("HTTP_HOST", "localhost")
 HTTP_PORT = int(os.getenv("HTTP_PORT", 8080))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "pushnstore")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "pushnstore")
+SECURE_FILENAME = int(os.getenv("SECURE_FILENAME", 1))
 
 
 app = Flask(__name__)
+
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -23,6 +25,21 @@ auth = HTTPBasicAuth()
 users = {
     ADMIN_USERNAME: generate_password_hash(ADMIN_PASSWORD)
 }
+
+
+def ensure_dir_exist(file_path: str):
+    """
+    Creates all necessary directories in the given file path if they don't exist.
+
+    Parameters:
+    - file_path (str): Full path to the file (including filename).
+
+    Example:
+    ensure_dir_exist("/path/to/some/folder/file.txt")
+    """
+    directory = os.path.dirname(file_path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 @auth.verify_password
@@ -41,8 +58,12 @@ def upload_file():
     if file.filename == "":
         return jsonify({"error": "No file selected"}), 400
 
-    filename = secure_filename(file.filename)
+    filename = file.filename
+    if SECURE_FILENAME == 1:
+        filename = secure_filename(file.filename)
+
     filepath = os.path.join(UPLOAD_FOLDER, filename)
+    ensure_dir_exist(filepath)
     file.save(filepath)
 
     return jsonify({"message": f"File {filename} uploaded successfully"}), 200
